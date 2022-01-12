4a93c08e0a13ebc8e79e2a0c8d006123c4f02864
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-13 01:42:27 +0500

    work complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { FileUpload } from './component/fileupload';
-import { FaReact } from 'react-icons/fa';
+import FileUpload from './components/FileUpload';
 import './App.css';
 
 const App = () => (
-    <div className="container mt-4">
-    <h1 className='display-4 text-center m4-4'>
-    <FaReact className='App-logo' /> React File upload
-    </h1>
-    <FileUpload />
-    </div>
-  );
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> React File Upload
+    </h4>
 
+    <FileUpload />
+  </div>
+);
 
 export default App;
