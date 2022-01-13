ca6df940117e0f83d07ac0b0788bb70109b5e8ff
fahadsidd107 <fsiddiqui107@gmail.com>
2022-01-14 03:09:44 +0500

    work complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import FileUpload from './components/FileUpload';
 import './App.css';
 
 const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
+  <div className='container mt-5'>
+    <h1 className='display-5 text-center mb-4'>
       <i className='fab fa-react' /> React File Upload
-    </h4>
+    </h1>
 
     <FileUpload />
   </div>
